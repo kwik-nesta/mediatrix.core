81bfb3dac43169733a2eab9760d80764008f5aaf
Toba Ojo <ojotobar@gmail.com>
2025-10-16 02:25:54 +0100

    Reorganize the code Bug fixes

---
+++ src/KwikNesta.Mediatrix.Hangfire/Abstractions/IKwikBackgroundMediator.cs
@@ -0,0 +1,10 @@
+﻿using KwikNesta.Mediatrix.Core.Abstractions;
+
+namespace KwikNesta.Mediatrix.Hangfire.Abstractions
+{
+    public interface IKwikBackgroundMediator
+    {
+        void Publish<TNotification>(TNotification notification) where TNotification : IKwikNotification;
+        void Send<TResponse>(IKwikRequest<TResponse> request);
+    }
+}
